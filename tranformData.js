6d02ba1d1fab2d7c23063b8567e9cfed6abdc98d
Calvin Chong <calvin@calvins-mbp.mynetworksettings.com>
2022-12-21 16:42:42 -0500

    Bulk Adding / ETL Process enabled for database upload

--- tranformData.js
+++ tranformData.js
@@ -1,38 +1,73 @@
-var fs = require ('fs');
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 
-const stream = fs.createReadStream("./questionsSample.csv");
-const reader = require("readline").createInterface({ input: stream })
+var fs = require ('fs');
+// var db = require ('./server/index.js');
+import { pdb } from './server/index.js';
+import { printTables, alterTable, showSchema } from './server/index.js';
+
+// printTables().
+//   then(res => {
+//     console.log('success');
+//   })
+
+
 
 let result = [];
-
-// reader.on('data', function(chunk) {
-//   console.log(chunk.toString());
-//   result.push(chunk.toString());
-//   console.log(result);
-//   // return chunk;
-// })
-
-// reader.on('line', function(chunk) {
-//   result.push(chunk);
-//   console.log(result);
-// })
-
 let count = 0;
 let lengthCheck = [];
-reader.on('line', function(chunk, splitRow = (result) => {
-  // result.map(el => {
-  //   return el.split(',');
-  // })
-  console.log(result);
-  console.log('this is the length', result.length);
-  lengthCheck.push(result.length);
-}) {
-    result.push(chunk.split(','));
-    count++;
-    splitRow(result);
-  }
-)
 
-setTimeout(() => {console.log(count)}, 2000);
-setTimeout(() => {console.log('this is the length', lengthCheck)}, 2000);
-// console.log(result);
+showSchema('product');
+
+setTimeout(() => {
+
+  const stream = fs.createReadStream("./productsSample.csv");
+  const reader = require("readline").createInterface({ input: stream })
+
+  reader.on('line', function(chunk) {
+    //.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g)
+  let entry = chunk.toString().match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g).filter(el => {
+    // console.log(el)
+    return el !== '' && el !== ',';
+  }).map( el => {
+    el = el.replace(/['"]+/g, '');
+    el = el.replace(/'/g, "\\'");
+    if (el.includes('"')) {
+      return el.slice(1, el.length - 1)
+    }
+    return el;
+  });
+  if (count < 8) {
+    // entry.forEach(el => console.log(el));
+    console.log('these are the entries',entry);
+  }
+  // result.push(chunk.split('","'));
+  result.push(chunk.split(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g));
+  count++;
+  if(count > 2 && count < 8) {
+    // console.log('this is the data to enter', entry);
+    // console.log('first', entry[0]);
+    // console.log('secon', entry[1]);
+    // console.log('third', entry[2]);
+    // console.log('4', entry[3]);
+    // console.log('5', entry[4]);
+    // console.log('price', entry[5]);
+    pdb.query(`INSERT INTO product (product_id, name, slogan, description, category, default_price) VALUES (` +
+      `${entry[0]}` + `,` +
+      `'` + `${entry[1]}` + `',` +
+      `'` + `${entry[2]}` + `',` +
+      `'` + `${entry[3]}` + `',` +
+      `'` + `${entry[4]}` + `',` +
+      `'` +`${entry[5]}` + `'` +
+    `)`
+    )
+  }
+})
+}, 2000) ;
+
+
+
+
+// Reads out whether it's been added.
+setTimeout(() => {console.log(count)}, 4000);
+setTimeout(() => {console.log('this is the length', lengthCheck)}, 4000);
